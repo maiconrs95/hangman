76d75a18e028ce6b02f6503665b79fd7d02250d1
Maicon Silva <maiconrs95@gmail.com>
2021-09-15 21:20:58 -0300

    feat: hangman game

---
+++ hangman.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+char palavrasecreta[20];
+char chutes[26];
+int tentativas = 0;
+
+void abertura()
+{
+    printf("/****************/\n");
+    printf("/ Jogo de Forca */\n");
+    printf("/****************/\n\n");
+}
+
+void chuta()
+{
+
+    char chute;
+    scanf(" %c", &chute);
+
+    chutes[tentativas] = chute;
+    tentativas++;
+}
+
+int jachutou(char letra)
+{
+    int achou = 0;
+
+    for (int j = 0; j < tentativas; j++)
+    {
+        if (chutes[j] == letra)
+        {
+            achou = 1;
+            break;
+        }
+    }
+
+    return achou;
+}
+
+void desenhaforca()
+{
+    for (int i = 0; i < strlen(palavrasecreta); i++)
+    {
+        int achou = jachutou(palavrasecreta[i]);
+
+        if (achou)
+        {
+            printf("%c ", palavrasecreta[i]);
+        }
+        else
+        {
+            printf("_ ");
+        }
+    }
+    printf("\n");
+}
+
+void escolhepalavra()
+{
+    sprintf(palavrasecreta, "MELANCIA");
+}
+
+int enforcou()
+{
+    int erros = 0;
+
+    for (int i = 0; i < tentativas; i++)
+    {
+        int existe = 0;
+
+        for (int j = 0; j < strlen(palavrasecreta); j++)
+        {
+            if (chutes[i] == palavrasecreta[j])
+            {
+                existe = 1;
+                break;
+            }
+        }
+
+        if (!existe)
+            erros++;
+    }
+
+    return erros >= 5;
+}
+
+int main()
+{
+
+    int acertou = 0;
+
+    escolhepalavra();
+    abertura();
+
+    do
+    {
+        desenhaforca();
+        chuta();
+    } while (!acertou && !enforcou());
+}
